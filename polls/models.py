6208c32c738ce76c658b7620853aa88f73d95947
dagusen <louisaleksieb.dagusen@gmail.com>
2018-04-16 16:43:07 +0800

    Add Cascading Style Sheet(css)

--- polls/models.py
+++ polls/models.py
@@ -10,7 +10,8 @@ class Question(models.Model):
     pub_date         = models.DateTimeField('date published')
 
     def __str__(self):
-        return self.question_text
+        return '{}'.format(self.question_text)
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
@@ -20,4 +21,4 @@ class Choice(models.Model):
     votes            = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return '{}'.format(self.choice_text)
