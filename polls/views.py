07cbd48709a4f665f0b40b9a7692a19ac6d78e8d
dagusen <louisaleksieb.dagusen@gmail.com>
2018-04-16 13:55:11 +0800

    If applied, this commit will be returning an HttpResponse object containing the content for the requested page, or raising an exception such as Http404. The rest is up to you.
    There’s a problem here, though: the page’s design is hard-coded in the view.

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,20 @@
 from django.http import HttpResponse
 
+from django.template import loader
+
 from django.shortcuts import render
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([a.question_text.count() in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
